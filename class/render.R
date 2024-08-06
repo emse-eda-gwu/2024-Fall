926e04ff84e28225df5935b6073af00c4f8731f9
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-08-06 16:39:43 +0000

    Built site for gh-pages

---
+++ class/render.R
@@ -0,0 +1,26 @@
+lesson <- strsplit(here::here(), "/")[[1]]
+lesson <- lesson[length(lesson)]
+
+# Build the slides
+renderthis::to_html("index.Rmd", "index.html")
+renderthis::to_pdf("index.html", paste0(lesson, ".pdf"))
+
+# Compress the PDF to reduce size
+tools::compactPDF(paste0(lesson, ".pdf"), gs_quality = 'ebook')
+
+# Create zip files of class notes
+zip::zip(
+    zipfile = paste0(lesson, ".zip"),
+    files = c(
+        'data',
+        'practice.qmd',
+        'practice-solutions.qmd',
+        'caseConverter_solution.R',
+        'caseConverter.R',
+        'internetUsers_solution.R',
+        'internetUsers.R',
+        'mpg.R',
+        'shinyWidgets.R',
+        'widgets.R',
+        paste0(lesson, ".Rproj"))
+)
